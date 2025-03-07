c2b61b07c295ec29006b2d7816eb075834ac052d
Alexander Cayetano <sr.alexco@gmail.com>
2025-03-07 12:19:50 -0500

    initial commit

---
+++ AppAlertsSwiftUI/AppAlertsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAlertsSwiftUIApp.swift
+//  AppAlertsSwiftUI
+//
+//  Created by alex on 7/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AppAlertsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppAlertsSwiftUITests/AppAlertsSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AppAlertsSwiftUITests.swift
+//  AppAlertsSwiftUITests
+//
+//  Created by alex on 7/03/25.
+//
+
+import Testing
+@testable import AppAlertsSwiftUI
+
+struct AppAlertsSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
